46b5aa2997aa9629afd41ffe0fb37f1a86413f88
purplemilycosmetic <purplemilycosmetic@gmail.com>
2025-06-11 01:06:33 +0800

    不知道按了啥 但好像沒大礙

--- 100check-website/src/main.js
+++ 100check-website/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store' // 導入 Vuex 儲存
 
 const app = createApp(App)
 app.use(router)
+app.use(store) // 使用 Vuex
 app.mount('#app')
